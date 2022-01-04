f535097adfebf30fb6cc19be5a2d01fc3be0396a
Mateusz Malinowski <malina314@op.pl>
2022-01-04 12:54:13 +0100

    sensor.h

---
+++ sensor.h
@@ -0,0 +1,10 @@
+#ifndef SENSOR_H
+#define SENSOR_H
+
+#include "position.h"
+
+class Sensor {
+    virtual bool is_safe([[maybe_unused]] coordinate_t x, [[maybe_unused]] coordinate_t y) = 0;
+};
+
+#endif //SENSOR_H
